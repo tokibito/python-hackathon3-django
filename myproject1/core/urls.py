79eae298807c585ca02b5b9a42ed0aaab3a46289
Shinya Okano <xxshss@yahoo.co.jp>
2009-11-14 15:17:29 +0900

    class based views

--- myproject1/core/urls.py
+++ myproject1/core/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
+from core.models import Person, Book
+from core.views import AddListViews
 
 urlpatterns = patterns('',
   url(r'^$', 'core.views.index', name='index'),
-  url(r'^person/$', 'core.views.list_person', name='list_person'),
-  url(r'^person/add$', 'core.views.add_person', name='add_person'),
-  url(r'^book/$', 'core.views.list_book', name='list_book'),
-  url(r'^book/add$', 'core.views.add_book', name='add_book'),
+  url(r'^person/', include(AddListViews(Person).urls)),
+  url(r'^book/', include(AddListViews(Book, '_book').urls)),
 )
--- myproject1/core/views.py
+++ myproject1/core/views.py
@@ -1,41 +1,66 @@
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.views.generic.simple import direct_to_template
+from django.conf.urls.defaults import *
+from django import forms
 
 from models import Person, Book
-from forms import PersonForm, BookForm
+
+class Views(object):
+    default_name = ''
+    
+    def __init__(self, name=None, app_name=None):
+        if name is None:
+            self.name = self.default_name
+        else:
+            self.name = name
+        if app_name is None:
+            self.app_name = self.default_name
+        else:
+            self.app_name = app_name
+
+    def get_urls(self):
+        return patterns('')
+
+    @property
+    def urls(self):
+        return self.get_urls(), self.app_name, self.name
+
+class AddListViews(Views):
+    def __init__(self, modelclass, template_name='', name=None, app_name=None):
+        super(AddListViews, self).__init__(name, app_name)
+        self.modelclass = modelclass
+        self.template_name = template_name
+        self.name = modelclass.__name__.lower()
+        self.app_name = self.name
+
+    def get_urls(self):
+        return patterns('',
+            url(r'^$', self.list_obj, name='list'),
+            url(r'^add$', self.add_obj, name='add'),
+        )
+
+    def list_obj(self, request):
+        return direct_to_template(request, 'list%s.html' % self.template_name, {
+          'objects': self.modelclass.objects.all(),
+          'title': self.modelclass._meta.verbose_name,
+        })
+
+    def make_form(self):
+        class ObjectForm(forms.ModelForm):
+            class Meta:
+                model = self.modelclass
+        return ObjectForm
+
+    def add_obj(self, request):
+        form = self.make_form()(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect('%s:list' % self.name)
+        return direct_to_template(request, 'add%s.html' % self.template_name, {
+          'form': form,
+          'title': self.modelclass._meta.verbose_name,
+        })
 
 def index(request):
     return direct_to_template(request, 'index.html')
-
-def list_person(request):
-    return direct_to_template(request, 'list.html', {
-      'objects': Person.objects.all(),
-      'title': Person._meta.verbose_name,
-    })
-
-def add_person(request):
-    form = PersonForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect(reverse('list_person'))
-    return direct_to_template(request, 'add.html', {
-      'form': form,
-      'title': Person._meta.verbose_name,
-    })
-
-def list_book(request):
-    return direct_to_template(request, 'list_book.html', {
-      'objects': Book.objects.all(),
-      'title': Book._meta.verbose_name,
-    })
-
-def add_book(request):
-    form = BookForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect(reverse('list_book'))
-    return direct_to_template(request, 'add_book.html', {
-      'form': form,
-      'title': Book._meta.verbose_name,
-    })
